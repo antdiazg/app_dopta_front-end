453011181e47b379b293c732eb33efebee82dcc2
Pipedsl <h.navarrete@duocuc.cl>
2024-05-06 12:51:08 -0400

    se agrega registro vista movil y desktop, y se ajustan los colores del proyecto y los estilo

--- src/app/auth/pages/registro/registro.page.ts
+++ src/app/auth/pages/registro/registro.page.ts
@@ -1,18 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.css'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    ReactiveFormsModule,]
 })
-export class RegistroPage{
+export class RegistroPage implements OnInit{
 
-  constructor() { }
+  formularioRegistro : FormGroup;
+  isMobilView!     : boolean;
 
+  constructor(public fb: FormBuilder) {
+    this.formularioRegistro = this.fb.group({
+      'username': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'confirmPassword': new FormControl("",Validators.required),
+      'email': new FormControl("",Validators.required),
+      'telefono': new FormControl("",Validators.required),
+      'direccion': new FormControl("",Validators.required),
+      'nombre': new FormControl("",Validators.required),
+      'apellido': new FormControl("",Validators.required),
+      'fechaNacimiento': new FormControl("",Validators.required),
+    }, {
+      validator: this.checkPasswords
+    })
+
+  }
+
+
+  ngOnInit(): void {
+      this.checkScreenWidth();
+      window.addEventListener('resize', () => {
+        this.checkScreenWidth();
+      });
+  }
+
+  checkScreenWidth(){
+    if (window.innerWidth <= 768) {
+      this.isMobilView = true;
+    } else{
+      this.isMobilView = false;
+    }
+  }
+
+  checkPasswords(group: FormGroup){
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : {notSame: true};
+  }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'principal-blue-400': '#55adfc',
+      'principal-blue-600': '#2272ee',
+      'pricipal-grey-400': '#808890',
+      'principal-grey-700': '#484C52',
+      'black': '#000000',
+      'indigo-700': 'rgb(67 56 202)',
+      'white': '#ffffff',
+      'blue-500': 'rgb(59 130 246)',
+      'errors-red-300': '#ff9898'
+
+    },
     extend: {},
   },
   plugins: [],
