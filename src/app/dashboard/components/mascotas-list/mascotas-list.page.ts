88656dba5de7b6e92f0a2d5022da0317d2e4477e
NicEspinozaT <nic.espinozat@duocuc.cl>
2024-06-17 23:38:24 -0400

    Quitando segundo scroll general

--- src/app/dashboard/components/mascotas-list/mascotas-list.page.ts
+++ src/app/dashboard/components/mascotas-list/mascotas-list.page.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { PublicationService } from 'src/app/shared/services/publication.service';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonButton } from '@ionic/angular/standalone';
 import { Mascota } from '../../Interfaces/mascota.interface';
+import { Router } from '@angular/router';
 import { User } from 'src/app/auth/interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-mascotas-list',
@@ -18,9 +20,11 @@ export class MascotasListPage implements OnInit {
 
   mascotas: Mascota[] = [];
   usuarios: User[] = [];
+  private router: Router = inject(Router);
 
   constructor(
     private publicationService: PublicationService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -33,6 +37,13 @@ export class MascotasListPage implements OnInit {
       this.mascotas = data;
 
     })
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
+  navigateTo(page: string) {
+    this.router.navigate([page]);
   }
 
 }
