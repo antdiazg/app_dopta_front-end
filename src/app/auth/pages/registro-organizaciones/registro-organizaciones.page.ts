59103ac06b6a695c528e226936cd20d6a726cc48
Pipedsl <h.navarrete@duocuc.cl>
2024-06-19 11:37:27 -0400

    se agregan alertas en registro persona, organización, y recuperar contraseña¨

--- src/app/auth/pages/registro-organizaciones/registro-organizaciones.page.ts
+++ src/app/auth/pages/registro-organizaciones/registro-organizaciones.page.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { environments } from 'src/environments/environment';
 import { Organizacion } from '../../interface';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -22,19 +23,20 @@ export class RegistroOrganizacionesPage implements OnInit {
     username: new FormControl<string>('', { nonNullable: true }),
     password: new FormControl<string>('', { nonNullable: true }),
     email: new FormControl<string>('', { nonNullable: true }),
-    telefono: new FormControl<number>(0, { nonNullable: true }),
+    telefono: new FormControl<number>(9, [Validators.minLength(9)]),
     direccion: new FormControl<string>('', { nonNullable: true }),
     numrut_org: new FormControl<number>(0, { nonNullable: true }),
     dv: new FormControl<string>('', { nonNullable: true }),
     razon_social: new FormControl<string>('', { nonNullable: true }),
-    telefono2: new FormControl<number>(0, { nonNullable: true }),
+    telefono2: new FormControl<number>(9, [Validators.minLength(9)]),
   });
   registroError: string = '';
   isMobilView!: boolean;
 
   constructor(
     @Inject(Router) private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private alertController: AlertController,
   ) { }
 
   get currentOrganization(): Organizacion {
@@ -74,14 +76,19 @@ export class RegistroOrganizacionesPage implements OnInit {
 
     this.authService.addOrganization(this.currentOrganization)
       .subscribe({
-        next: (RegisterResponse) => {
+        next: async (RegisterResponse) => {
           console.log('Registro succesful!', RegisterResponse);
-          window.location.href = `${environments.BASE_URL}/auth/login/`
-          this.formularioRegistro.reset();
+          await this.presentWelcomeAlert();
         },
-        error: (error) => {
-          console.error('Error al registrar la organización', error);
-          this.registroError = 'Registro organización invalido';
+        error: async (error) => {
+          console.error('Registro error', error);
+          if (error.status === 400) {
+            this.presentErrorAlert('El correo o usuario ya se encuentra registrado.');
+            this.registroError = 'Registro organización invalido';
+          }else{
+            this.presentErrorAlert('Error al registrar la organización.');
+            this.registroError = 'Registro organización invalido';
+          }
         }
 
       })
@@ -95,10 +102,62 @@ export class RegistroOrganizacionesPage implements OnInit {
     }
   }
 
+  get telefono() {
+    return this.formularioRegistro.get('telefono');
+  }
+
   checkPasswords(group: FormGroup) {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
 
     return password === confirmPassword ? null : { notSame: true };
+  }
+
+  async presentWelcomeAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Registro Exitoso!',
+      message: 'Se ha enviado un enlace de activación al correo registrado, por favor activa tu cuenta para poder iniciar sesión.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Alerta cerrada por el usuario.');
+            this.navigateToHome(); // Redirigir después de cerrar la alerta
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    setTimeout(() => {
+      alert.dismiss().then(() => {
+        console.log('Alerta cerrada automáticamente.');
+        this.navigateToHome(); // Redirigir después de cerrar automáticamente
+      });
+    }, 10000); // 5000 milisegundos = 5 segundos
+
+  }
+
+  async presentErrorAlert(message: string): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Error al registrar',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    // Cerrar la alerta automáticamente después de 5 segundos (ajusta el tiempo según tus necesidades)
+    setTimeout(() => {
+      alert.dismiss().then(() => {
+        console.log('Alerta de error cerrada automáticamente.');
+      });
+    }, 10000); // 5000 milisegundos = 5 segundos
+  }
+
+  navigateToHome(): void {
+    // Redirige a la página principal después de cerrar la alerta
+    window.location.href = `${environments.BASE_URL}`;
+    this.formularioRegistro.reset();
   }
 }
