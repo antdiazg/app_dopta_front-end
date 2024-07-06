8d2b89951b4e4b31b9d047610a99af416fc731d8
NicEspinozaT <nic.espinozat@duocuc.cl>
2024-07-05 22:34:21 -0400

    Agregando 'X' para cerrar detalle mascota

--- src/app/dashboard/components/detalle-mascota/detalle-mascota.component.ts
+++ src/app/dashboard/components/detalle-mascota/detalle-mascota.component.ts
@@ -1,21 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController, } from '@ionic/angular';
 import { Mascota } from 'src/app/dashboard/Interfaces/mascota.interface';
 import { CommonModule } from '@angular/common';
-import { IonContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent, IonCard } from "@ionic/angular/standalone";
+import { IonContent, IonButton, IonButtons, IonIcon, IonToolbar, IonHeader, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonTitle } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { close } from 'ionicons/icons';
 
 @Component({
   selector: 'app-detalle-mascota',
   templateUrl: './detalle-mascota.component.html',
   styleUrls: ['./detalle-mascota.component.css'],
   standalone: true,
-  imports: [IonCard, IonContent, CommonModule, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent]
+  imports: [IonTitle,
+    CommonModule,
+    IonCard,
+    IonContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+    IonButton,
+    IonButtons,
+    IonIcon,
+    IonToolbar,
+    IonHeader
+  ]
 })
 export class DetalleMascotaComponent implements OnInit {
   @Input() mascota!: Mascota;
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor(private modalCtrl: ModalController) {
+    addIcons({
+      'close': close,
+    });
   }
 
+  ngOnInit() { }
+
+  cerrarModal() {
+    this.modalCtrl.dismiss();
+  }
 }
